c46f683be6e9c2415b9fe8246b7bcc48a1b28581
artem19140 <artrl0504@gmai..com>
2026-06-08 11:21:22 +0400

    new commands fixed

--- app/Console/Commands/FrontendDeploy.php
+++ app/Console/Commands/FrontendDeploy.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\Process\Process;
 
 #[Signature('app:frontend-deploy')]
@@ -14,22 +15,31 @@ class FrontendDeploy extends Command
     public function handle()
     {
         $path = '/var/www/tigr';
+
         $commands = [
             ['npm', 'ci'],
             ['npm', 'run', 'build']
         ];
+
         $this->info("Выполняем frontend deploy...");
+        Log::info("Выполняем frontend deploy...");
+
         foreach($commands as $command){
+            $commandImploded = implode(' ', $command);
+
             $process = new Process($command ,$path);
             $process->run();
 
             if (!$process->isSuccessful()) {
-                $this->error("npm" . $process->getErrorOutput());
+                $this->error("npm ($commandImploded)" . $process->getErrorOutput());
+                Log::warning("npm ($commandImploded)" . $process->getErrorOutput());
                 return 1;
             }
 
             $this->info($process->getOutput());
-            $this->info('npm команты успешно завершены');
+            Log::warning("npm ($commandImploded)" . $process->getOutput());
+            
         }
+        $this->info('npm команты успешно завершены');
     }
 }
--- app/Console/Commands/GitPull.php
+++ app/Console/Commands/GitPull.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\Process\Process;
 
 #[Signature('app:git-pull')]
@@ -17,15 +18,19 @@ class GitPull extends Command
     {
         $path = '/var/www/tigr';
         $this->info("git pull...");
+        Log::info("git pull...");
         $process = new Process(['git', 'pull'], $path);
         $process->run();
 
         if (!$process->isSuccessful()) {
             $this->error("Git pull failed: " . $process->getErrorOutput());
+            Log::warning("Git pull failed: " . $process->getErrorOutput());
             return 1;
         }
 
         $this->info($process->getOutput());
+        
+        Log::info('изменения получены');
         $this->info('изменения получены');
     }
 }
